e1ab982ee9edc4429c151848c2198a5296e8f080
立ち読み <naonosuke311@yahoo.co.jp>
2018-04-28 23:43:57 +0900

    4/28-2

--- MapCreate/MapCreate/Main.cpp
+++ MapCreate/MapCreate/Main.cpp
@@ -8,7 +8,7 @@ Point getRandomPoint(double radius)
 	for (int i = 0; i < 5; i++)
 		r += radius*Random();
 	r /= 5;
-	return Point(tileSize.x*ceil(r*cos(theta) / tileSize.x), tileSize.y*ceil(r*sin(theta) / tileSize.y));
+	return Point(tileSize.x*ceil(r*cos(theta) / tileSize.x+1), tileSize.y*ceil(r*sin(theta) / tileSize.y+1));
 }
 
 void addRect(int r, std::vector<Rect>& rooms)
@@ -30,27 +30,35 @@ void addRect(int r, std::vector<Rect>& rooms)
 
 void Main()
 {
-	Window::Resize(1500, 600);
+	Window::Resize(1000, 1000);
 	const int r = 200;
 	const Circle circle(r);
 
 
-	const int roomNum = 30;
+	const int roomNum = 50;
 	std::vector<Rect> roomRect;
 	for (int i = 0; i < roomNum; i++)
 		addRect(r, roomRect);
 
+	Point centerPos = Window::Center();
 	size_t nowNumber = 0;
 	bool next = false;
 
 	while (System::Update())
 	{
+		if (Input::MouseL.clicked)
+			centerPos = Mouse::Pos();
+
 		int k = 0;
 		for (size_t i = 0; i < roomNum; i++)
 		{
 			if (roomRect[nowNumber].intersects(roomRect[i]) && i != nowNumber)
 			{
-				roomRect[nowNumber].moveBy(roomRect[nowNumber].center.x > 0 ? tileSize.x : -tileSize.x, 0);// roomRect[nowNumber].center.y > 0 ? tileSize.y : -tileSize.y);
+				if (abs(roomRect[nowNumber].center.x) > abs(roomRect[nowNumber].center.y))
+					roomRect[nowNumber].moveBy(roomRect[nowNumber].center.x > 0 ? tileSize.x : -tileSize.x, 0);// roomRect[nowNumber].center.y > 0 ? tileSize.y : -tileSize.y);
+				else
+					roomRect[nowNumber].moveBy(0,roomRect[nowNumber].center.x > 0 ? tileSize.y : -tileSize.y);// roomRect[nowNumber].center.y > 0 ? tileSize.y : -tileSize.y);
+				
 				k++;
 			}
 		}
@@ -63,7 +71,7 @@ void Main()
 
 
 		{
-			const Transformer2D transformer(Mat3x2::Scale(0.8).translate(Window::Center()));
+			const Transformer2D transformer(Mat3x2::Scale(0.5).translate(centerPos));
 			//circle.drawFrame(2.0, 0.0, Color(Palette::White, 200));
 			for (auto e : roomRect)
 			{
